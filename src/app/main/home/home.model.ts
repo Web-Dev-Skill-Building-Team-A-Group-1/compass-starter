a4af8286e9b73977b6b7da8c3c06e6c3b6fcb396
Jaycob <jpakinga@ucsc.edu>
2026-07-30 13:48:44 -0700

    feat: implement Weekly Goals Item

--- src/app/main/home/home.model.ts
+++ src/app/main/home/home.model.ts
@@ -1,1 +1,6 @@
 // Add any extra data types you'll need here!
+import { WeeklyGoal } from '../../core/store/weekly-goal/weekly-goal.model';
+import { Hashtag } from '../../core/store/hashtag/hashtag.model';
+
+export interface WeeklyGoalData extends WeeklyGoal{ hashtag: Hashtag }
+
--- src/app/main/home/weekly-goals/weekly-goals.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals.component.ts
@@ -3,6 +3,10 @@ import { WeeklyGoalsAnimations } from './weekly-goals.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
+import { WeeklyGoalsItemComponent } from './weekly-goals-item/weekly-goals-item.component';
+import { Timestamp } from '@angular/fire/firestore';
+import { WeeklyGoalData } from '../home.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-weekly-goals',
@@ -11,34 +15,64 @@ import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: WeeklyGoalsAnimations,
   standalone: true,
-  imports: [
-  ],
+  imports: [WeeklyGoalsItemComponent],
 })
-export class WeeklyGoalsComponent implements OnInit {
-  readonly authStore = inject(AuthStore);
+export class WeeklyGoalsComponent implements OnInit {  
   // --------------- INPUTS AND OUTPUTS ------------------
-
-  /** The current signed in user. */
-  currentUser: Signal<User> = this.authStore.user;
 
   // --------------- LOCAL UI STATE ----------------------
 
-  /** Loading icon. */
-  loading: WritableSignal<boolean> = signal(false);
+  sampleData: WeeklyGoalData = {
+    __id: 'wg1',
+    __userId: 'test-user',
+    __quarterlyGoalId: 'qg1',
+    __hashtagId: 'ht1',
+    text: 'Apply to Microsoft',
+    completed: false,
+    order: 1,
+    _createdAt: Timestamp.now(),
+    _updatedAt: Timestamp.now(),
+    _deleted: false,
+    hashtag: {
+      __id: 'ht1',
+      __userId: 'test-user',
+      name: 'apply-internships',
+      color: '#2DBDB1',
+      _createdAt: Timestamp.now(),
+      _updatedAt: Timestamp.now(),
+      _deleted: false,
+    },
+  };
 
+  
   // --------------- COMPUTED DATA -----------------------
 
+
   // --------------- EVENT HANDLING ----------------------
+  completion = "incomplete";
+  checkGoal(newCheckState: boolean) {
+    if( newCheckState === true){
+        this.completion = "incomplete"
+    } else {
+        this.completion = "complete"
+    }
+    this.snackBar.open(
+      'Clicked on checkbox to change state to: ' + this.completion,
+      '',
+      {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      },
+    );
+  }
+
 
   // --------------- OTHER -------------------------------
 
-  constructor(
-    private injector: Injector,
-    @Inject(BATCH_WRITE_SERVICE) private batch: BatchWriteService,
-  ) { }
+  constructor(private snackBar: MatSnackBar) {}
 
   // --------------- LOAD AND CLEANUP --------------------
   
-  ngOnInit(): void {
-  }
+  ngOnInit() {}
 }
